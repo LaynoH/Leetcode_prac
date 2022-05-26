5dcc7ba7551f9b5e95a056f4383cdd511d29d449
Yi-Chen Chiang <elainechiang99@gmail.com>
2022-05-26 16:44:41 -0400

    Update #07 Reverse Integer.py

--- medium/#07 Reverse Integer.py
+++ medium/#07 Reverse Integer.py
@@ -7,16 +7,16 @@ class Solution:
     def reverse(self, x: int) -> int:
         if Solution.check(x)==0:
             return 0
-        if x ==0:
+        if x == 0:
             return x
+        
+        sign = 1
         if x < 0:
-            strX = '-'+str(x)[-1:0:-1]
-            x = int(strX.lstrip('0'))           #newly learned: .lstrip()
-            if Solution.check(x)==0:
+            sign = -1
+            x = x*sign
+        x = int(str(x)[-1::-1].lstrip('0'))
+        if Solution.check(x)==0:
                 return 0
-            return x
         else:
-            x = int(str(x)[-1::-1].lstrip('0'))
-            if Solution.check(x)==0:
-                return 0
-            return x
+            return sign*x
+
