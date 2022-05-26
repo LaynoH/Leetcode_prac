d134c0f214bbd978705f6d44b81ef6cd968b3d3f
Yi-Chen Chiang <elainechiang99@gmail.com>
2022-05-26 17:21:10 -0400

    Update #29 Divide Two Integers.py

--- medium/#29 Divide Two Integers.py
+++ medium/#29 Divide Two Integers.py
@@ -1,17 +1,8 @@
 class Solution:
-    def check(x):
-        if x < -(2**31) or x > (2**31-1):
-            return 0
-        else:
-            return 1
     def divide(self, dividend: int, divisor: int) -> int:
-        if Solution.check(dividend) == 0:
-            return dividend
         if divisor!=0:
             res = int(dividend/divisor)
-            if Solution.check(res) == 0:
-                if res > 0:
-                    return (2**31-1)
-                else:
-                    return -(2**31)
-            return res
+            if res > 0:
+                return min(res, (2**31-1))
+            else:
+                return max(res, -(2**31))
