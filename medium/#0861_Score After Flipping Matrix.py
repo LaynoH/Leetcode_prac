6ebba4a9b59aa210d5a861007d496b288135a0e3
Yi-Chen Chiang <elainechiang99@gmail.com>
2024-05-13 15:42:42 -0400

    Update #0861_Score After Flipping Matrix.py

--- medium/#0861_Score After Flipping Matrix.py
+++ medium/#0861_Score After Flipping Matrix.py
@@ -10,10 +10,13 @@ class Solution:
         trans = list(zip(*grid))
 
         for i in range(1, n):
+            # 2^(pos_val)
             pos_val = 1 << (n-1-i)
+            # get difference between first column and others
             count_1 = len([j for j,k in zip(trans[0], trans[i]) if j^k])
             output += max(count_1, m-count_1) * pos_val  
         return output
+                
 
 # ref: https://leetcode.com/problems/score-after-flipping-matrix/solutions/5149964/fastest-100-easy-to-understand-clean-concise
 # ref: https://leetcode.com/problems/score-after-flipping-matrix/solutions/5150856/think-in-terms-of-bit-positions-java-c
